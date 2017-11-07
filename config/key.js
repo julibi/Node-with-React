9fff7121442119e43597e9e37f083b293f908c35
Hyun-Kyung Yi <hyun.yi@nb-hyi.xing.hh>
2017-11-07 20:27:31 +0100

    ...

--- config/key.js
+++ config/key.js
@@ -1,4 +1,5 @@
 module.exports = {
   googleClientID: '835975247095-paui5uu9kvtt2sra1hav20i3onq66bmg.apps.googleusercontent.com',
-  googleClientSecret: 'budUgArQYwYKbcHVN8ZjkFsQ'
+  googleClientSecret: 'budUgArQYwYKbcHVN8ZjkFsQ',
+  mongoURI: 'mongodb://user2:user2@ds029187.mlab.com:29187/emaily-dev'
 };
