536092754b39074994a9acc42cbf7b85723b1ce3
Hyun-Kyung Yi <hyun.yi@nb-hyi.xing.hh>
2017-11-08 22:06:00 +0100

    backend setup and passport

--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,9 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const mongoose = require('mongoose');
-const keys = require('../config/key');
-require ('../models/User');
-const User = mongoose.model('users');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/key");
+require("../models/User");
+const User = mongoose.model("users");
 
 //take user model and put identifying info into a cookie
 passport.serializeUser((user, done) => {
@@ -11,38 +11,37 @@ passport.serializeUser((user, done) => {
   //but instead is the shortcut to mongos auto-generated ID
   //after user hs signed in with profile.id we don't care about it anymore
   //we only care about our homemade user.id for rest of the app flow
-  done(null, user.id)
+  done(null, user.id);
 });
 
 //pull the info back out and return the user
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
-passport.use(new GoogleStrategy(
-  {
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
-    //this query returns a promise
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if(existingUser) {
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      //this query returns a promise
+     const existingUser = await User.findOne({ googleId: profile.id })
+        if (existingUser) {
           //we already have a record with the given profile id
           //done tells passport we are done
           done(null, existingUser);
         } else {
-            new User({ googleId: profile.id })
-              .save()
-              //'user' is the fresh instance of 'User'
-              //and is what we use up in serializeUser
-              .then(user => done(null, user));
+          const user = await new User({ googleId: profile.id }).save()
+            //'user' is the fresh instance of 'User'
+            //and is what we use up in serializeUser
+            done(null, user);
         }
-      })
-  }
-));
+    }
+  )
+);
